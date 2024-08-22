f929fbb505404dbbd16247cb557907baac18416e
Rohith Gundu <Rohith.Gundu@godigit.com>
2024-08-22 14:49:28 +0530

    implemented the register functionality

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup,Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { HttpService } from 'src/app/services/http.service';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +9,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
+  regdata!:FormGroup;
+  constructor(private fb:FormBuilder,private httpservice:HttpService,private router:Router){}
+  ngOnInit(): void {
+    this.regdata= this.fb.group({
+      Name:["",[Validators.required,Validators.minLength(3)]],
+      Email:["",[Validators.required,Validators.email]],
+      role:["",[Validators.required]],
+      city:["",[Validators.required]],
+      state:["",[Validators.required]],
+      pin:["",[Validators.required,Validators.minLength(6),Validators.maxLength(6)]],
+  
+    })
+  }
+
+  handelRegister(){
+    if(this.regdata.invalid)return ;
+    console.log(this.regdata.value);
+    const type=this.regdata.value.role;
+    console.log(type);
+    if(type=="student"){
+      this.httpservice.poststudentdata(this.regdata.value).subscribe(()=>{
+        next:()=>{console.log("success");}
+        console.log("success");
+        this.router.navigate(['/dashboard']);
+      })
+    }
+    if(type=="teacher"){
+      this.httpservice.postteacherdata(this.regdata.value).subscribe(()=>{
+        next:()=>{console.log("success");}
+        console.log("success");
+        this.router.navigate(['/dashboard']);
+      })
+    }
+    
+  }
 
 }
