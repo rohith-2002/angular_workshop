d21d7bbf247abb80f6cd267e1933941342472705
Rohith Gundu <Rohith.Gundu@godigit.com>
2024-08-22 18:38:01 +0530

    implemented the teacher data diaplay

--- src/app/components/teacher-data/teacher-data.component.ts
+++ src/app/components/teacher-data/teacher-data.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
-
+import { Component, Input } from '@angular/core';
+import { HttpService } from '../../services/http.service';
+import { DataService } from '../../services/data.service';
 @Component({
   selector: 'app-teacher-data',
   templateUrl: './teacher-data.component.html',
   styleUrls: ['./teacher-data.component.scss']
 })
 export class TeacherDataComponent {
+   @Input() SearchText:string=''; 
+    teacherdata:any[]=[];
+    constructor(private httpservice:HttpService,private dataservice:DataService){
+        this.httpservice.getteacherdata().subscribe({
+              next:(res:any)=>{
+                this.teacherdata=res;
+                console.log(this.teacherdata);
+              },error:(err:any)=>{
+                console.log(err);
+              }
+        })
 
+        this.dataservice.currenttext.subscribe((message) => {
+          this.SearchText = message ;
+    })
+  }
 }
