8eb8137d8dae468b1e5147a550921e37bea76e2f
Rohith Gundu <Rohith.Gundu@godigit.com>
2024-08-22 18:37:12 +0530

    implemented the function for the displaying of student data

--- src/app/components/student-data/student-data.component.ts
+++ src/app/components/student-data/student-data.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { HttpService } from '../../services/http.service';
+import { Input } from '@angular/core';
+import { DataService } from '../../services/data.service';
 
 @Component({
   selector: 'app-student-data',
@@ -6,5 +9,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./student-data.component.scss']
 })
 export class StudentDataComponent {
+  @Input() SearchText:string='';
+  studentdata:any[]=[];
+  constructor(private httpservice:HttpService,private dataservice:DataService){
+      this.httpservice.getstudentdata().subscribe({
+            next:(res:any)=>{
+              this.studentdata=res;
+              console.log(this.studentdata);
+            },error:(err:any)=>{
+              console.log(err);
+            }
+      });
+      this.dataservice.currenttext.subscribe((message) => {
+        this.SearchText =message ;
+  });
+
+
+  }
+
+
+
 
 }
